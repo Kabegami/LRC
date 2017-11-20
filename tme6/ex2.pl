91c254561ff31db75be621d0e8c480cba502554d
Kabegami <lucas.becirspahic@gmail.com>
2017-11-20 18:00:36 +0100

    tme6

---
+++ tme6/ex2.pl
@@ -0,0 +1,17 @@
+concatene([],L,L).
+concatene([T | L1], L2, [T | R]) :-concatene(L1, L2, R).
+
+inverse([],[]).
+inverse([A | L1], Y) :-  inverse(L1, L2), concatene(L2, [A], Y).
+
+supprime([],X,[]).
+supprime([X | L], X, Res) :- supprime(L, X, Res).
+supprime([T | L], X, Res) :- T \= X, supprime(L, X, S), concatene([T], S, Res).
+
+filtre([],L2,[]).
+filtre(L1, [], L1).
+filtre(L1, [X | L2], L3) :- supprime(L1, X, S), filtre(S, L2, L3).
+
+palindrome(X) :- inverse(X,X).
+
+dernier(X, L) :- concatene(L2, X, L).
